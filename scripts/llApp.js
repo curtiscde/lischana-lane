c4bd089e9c0ae965d2ca1f93797bb3ed4ec61c4c
CurtTimson <curt@live.co.uk>
2017-02-11 18:39:58 +0000

    Include initial AngularJs routes

--- scripts/llApp.js
+++ scripts/llApp.js
@@ -1,6 +1,36 @@
-var llApp = angular.module("llApp", []);
+'use strict';
+
+var llApp = angular.module("llApp", ['ngRoute']);
 
 llApp.controller("llCtrl", function($scope, $http){
+
+});
+
+llApp.config(function ($routeProvider,  $locationProvider) {
+    // configure the routes
+
+    $routeProvider
+    .when('/', {
+    // route for the home page
+        templateUrl: 'home.html',
+        controller: 'homeController'
+    })
+
+    .when('/about', {
+    // route for the about page
+        templateUrl: 'about.html',
+        controller: 'aboutController'
+    })
+    .otherwise({
+        templateUrl: 'routeNotFound.html',
+       controller: 'notFoundController'
+    });
+
+    $locationProvider.html5Mode(true);
+
+});
+
+llApp.controller("homeController", function($scope, $http, $route){
 
 
 });
