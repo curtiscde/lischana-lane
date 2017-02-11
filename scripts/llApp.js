d04082c2c5e48a8e76e3642834e5393353561a5c
CurtTimson <curt@live.co.uk>
2017-02-11 19:20:20 +0000

    Remove route logic

--- scripts/llApp.js
+++ scripts/llApp.js
@@ -3,32 +3,5 @@
 var llApp = angular.module("llApp", ['ngRoute'])
 
 .controller('MainController', function($scope, $route, $routeParams, $location) {
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
-})
-
-.controller('FilmController', function($scope, $routeParams) {
-    $scope.name = 'FilmController';
-    $scope.params = $routeParams;
-})
-
-.config(function($routeProvider, $locationProvider) {
-  $routeProvider
-   .when('film', {
-    templateUrl: 'film.html',
-    controller: 'FilmController',
-    resolve: {
-      // I will cause a 1 second delay
-      delay: function($q, $timeout) {
-        var delay = $q.defer();
-        $timeout(delay.resolve, 1000);
-        return delay.promise;
-      }
-    }
-  });
-
-  $locationProvider.html5Mode({
-    enabled: true
-  });
+    
 });
