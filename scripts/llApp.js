a22adf53a7fab7726606b37173ff44e8ad138183
CurtTimson <curt@live.co.uk>
2017-02-11 19:43:01 +0000

    Reinstate routing and include film page

--- scripts/llApp.js
+++ scripts/llApp.js
@@ -2,6 +2,26 @@
 
 var llApp = angular.module("llApp", ['ngRoute'])
 
-.controller('MainController', function($scope, $route, $routeParams, $location) {
-    
+.controller('mainController', function($scope, $route, $routeParams, $location) {
+
+});
+
+// configure our routes
+llApp.config(function($routeProvider) {
+  $routeProvider
+
+    .when('/', {
+      templateUrl : 'home.html',
+      controller  : 'mainController'
+    })
+
+    .when('/film', {
+      templateUrl : 'film.html',
+      controller  : 'filmController'
+    })
+
+});
+
+llApp.controller('filmController', function($scope) {
+  $scope.name = 'Look! I am an about page.';
 });
