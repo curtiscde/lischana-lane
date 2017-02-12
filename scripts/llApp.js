654153e20737926664256b3fd26da524c459ffae
CurtTimson <curt@live.co.uk>
2017-02-12 12:44:52 +0000

    Bind film data to view

--- scripts/llApp.js
+++ scripts/llApp.js
@@ -22,15 +22,26 @@ llApp.config(function($routeProvider) {
 
 });
 
-llApp.controller('filmController', function($scope, $http, $timeout) {
+llApp.controller('filmController', function($scope, $http, $timeout, $sce) {
 
   $http.get("data/film.json").then(function(response){
     console.log(response.data.films); //TODO: Remove
 
-    $scope.films = response.data.films;
+    $scope.films = mapFilmJsonToModel(response.data.films);
 
   }, function(){
     console.log("error");
   });
+
+  var mapFilmJsonToModel = function(data){
+    var films = [];
+    for(let i = 0; i<data.length; i++){
+      let film = data[i];
+      films.push({
+        src: $sce.trustAsResourceUrl(film.src)
+      });
+    }
+    return films;
+  };
 
 });
