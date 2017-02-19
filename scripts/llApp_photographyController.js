2105d902762705b9af1eb5d321a0313c34299668
CurtTimson <curt@live.co.uk>
2017-02-19 19:52:03 +0000

    Pull data from photos JSON and filter by section

--- scripts/llApp_photographyController.js
+++ scripts/llApp_photographyController.js
@@ -1,5 +1,27 @@
-llApp.controller('photographyController', function($scope, $routeParams) {
+llApp.controller('photographyController', function($scope, $routeParams, $http) {
 
   var section = $routeParams.section;
+
+  $http.get("data/photography.json").then(function(response){
+
+    $scope.photos = mapPhotoJsonToModel(response.data.photos, section);
+
+    console.log($scope.photos); //TODO: Remove
+
+  }, function(){
+    console.log("error");
+  });
+
+  var mapPhotoJsonToModel = function(data, section){
+    var photos = [];
+    data.filter(function(photo){
+      return photo.sections.indexOf(section)>=0;
+    }).map(function(i,v){
+      photos.push({
+        file: i.file
+      });
+    });
+    return photos;
+  };
 
 });
