27b5a71ec7d8e21d7d692f219a73fb5d4644c762
CurtTimson <curt@live.co.uk>
2017-03-07 18:42:35 +0000

    Add gulp publish and deploy tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
+var ghPages = require('gulp-gh-pages');
 
 var stylePath = "style/**/*.less";
 var lessTask = "less";
@@ -12,4 +13,16 @@ gulp.task(lessTask, function(){
 
 gulp.task('less-watch', function(){
   gulp.watch(stylePath, [lessTask]);
+});
+
+gulp.task('publish', function(){
+  return gulp.src(['**/**', '!dist', '!node_modules'])
+    // .pipe(useref())
+    // .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
 });
