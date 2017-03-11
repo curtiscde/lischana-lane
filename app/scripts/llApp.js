719beed724a706eea516e32f770d0a925fa7e45e
CurtTimson <curt@live.co.uk>
2017-03-11 15:50:04 +0000

    Remove commented out routing logic

--- app/scripts/llApp.js
+++ app/scripts/llApp.js
@@ -1,28 +1,5 @@
 'use strict';
 var ll = ll || {};
 ll.llApp = angular.module("llApp", ['ngRoute'])
-
 .controller('mainController', function($scope, $route, $routeParams, $location) {
-
 });
-
-// // configure our routes
-// ll.llApp.config(function($routeProvider) {
-//   $routeProvider
-//
-//     .when('/', {
-//       templateUrl : 'home.html',
-//       controller  : 'mainController'
-//     })
-//
-//     .when('/film', {
-//       templateUrl : 'film.html',
-//       controller  : 'filmController'
-//     })
-//
-//     .when('/photography/:section', {
-//       templateUrl : 'photography.html',
-//       controller  : 'photographyController'
-//     });
-//
-// });
