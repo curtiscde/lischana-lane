e60936c5bff695d84291cf9177ce5727d9ef36da
CurtTimson <curt@live.co.uk>
2017-03-11 15:58:46 +0000

    Add anchor scroll to sections

--- app/scripts/llApp.js
+++ app/scripts/llApp.js
@@ -1,5 +1,12 @@
 'use strict';
 var ll = ll || {};
 ll.llApp = angular.module("llApp", ['ngRoute'])
-.controller('mainController', function($scope, $route, $routeParams, $location) {
+.controller('mainController', function($scope, $route, $routeParams, $location, $anchorScroll) {
+
+  $scope.scrollTo = function(id) {
+      $anchorScroll.yOffset = $(".logo-spacer").outerHeight(true);
+      $location.hash(id);
+      $anchorScroll();
+   };
+
 });
