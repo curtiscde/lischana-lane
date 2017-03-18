ff781d42cf73b8ed69b568326b73028133276b81
CurtTimson <curt@live.co.uk>
2017-03-18 18:32:58 +0000

    Added location.hash update

--- app/scripts/llApp.js
+++ app/scripts/llApp.js
@@ -4,8 +4,7 @@ ll.llApp = angular.module("llApp", ['ngRoute'])
 .controller('mainController', function($scope, $route, $routeParams, $location, anchorSmoothScroll) {
 
   $scope.scrollTo = function(id) {
-      //$anchorScroll.yOffset = $(".sticky-spacer").outerHeight(true);
-      //$location.hash(id);
+      $location.hash(id);
       anchorSmoothScroll.setOffset($(".sticky-spacer").outerHeight(true));
       anchorSmoothScroll.scrollTo(id);
    };
