5344a8eb2ac1ee14716b5952863e7a19a7dd8b1b
Curtis Timson <curt@live.co.uk>
2019-07-08 18:22:41 +0100

    Remove lastfm

--- static/ll/js/ng-app.js
+++ static/ll/js/ng-app.js
@@ -1,18 +1,3 @@
-angular.module('ll', ['ll.tweet', 'lastfm-nowplaying', 'ngSanitize'])
-  .controller('mainCtrl', ['$scope', '$sce', 'tweetService', function($scope, $sce, tweetService){
-
-      tweetService.getTweet().then(function(data){
-        $scope.tweet = $sce.trustAsHtml(data);
-      });
-
-      $scope.lastFmConfig = {
-        apiKey: 'f3c3bb60dc23d1431a15c557e1db8de6',
-        user: 'lvel',
-        containerClass: 'container lastfm-content'
-      };
-
-  }]);
-
 angular.module('ll.tweet', [])
   .factory('tweetService', function($q, $http){
 
