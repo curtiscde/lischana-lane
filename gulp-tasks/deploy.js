4bbdb80388dd9f8a454051591aaab6fa1624c0f8
curtiscde <curt@live.co.uk>
2022-06-09 08:39:32 +0100

    🔥 Remove gh-pages deploy methods

--- gulp-tasks/deploy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var ghPages = require('gulp-gh-pages');
-
-gulp.task('deploy', function() {
-  return gulp.src('./public/**/*')
-    .pipe(ghPages({
-      "remoteUrl": "git@github.com:curttimson/lischana-lane.git"
-    }));
-});
