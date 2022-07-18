f24f8f4e8b52574e13f323b6455d4627875e0eb9
Curtis Timson <curt@live.co.uk>
2022-07-18 18:01:33 +0100

    ✨ Big Picture theme (#48)

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" href="assets/css/main.css" />
+          <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+        </Head>
+        <body className='is-preload'>
+          <Main />
+          <NextScript />
+          <Script src="assets/js/jquery.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/jquery.poptrox.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/jquery.scrolly.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/jquery.scrollex.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/browser.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/breakpoints.min.js" strategy='beforeInteractive' />
+          <Script src="assets/js/util.js" strategy='beforeInteractive' />
+          <Script src="assets/js/main.js" strategy='beforeInteractive' />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
