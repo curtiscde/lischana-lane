6d87318e86176853b84aa2490e7a0ca47c652412
curtiscde <curt@live.co.uk>
2022-07-18 18:30:59 +0100

    🚨 Linting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import Document, {
+  DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript,
+} from 'next/document';
+import Script from 'next/script';
+import React from 'react';
 
 class MyDocument extends Document {
   static async getInitialProps(
-    ctx: DocumentContext
+    ctx: DocumentContext,
   ): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx)
+    const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
+    return initialProps;
   }
 
   render() {
@@ -17,21 +20,21 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="assets/css/main.css" />
           <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
         </Head>
-        <body className='is-preload'>
+        <body className="is-preload">
           <Main />
           <NextScript />
-          <Script src="assets/js/jquery.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/jquery.poptrox.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/jquery.scrolly.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/jquery.scrollex.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/browser.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/breakpoints.min.js" strategy='beforeInteractive' />
-          <Script src="assets/js/util.js" strategy='beforeInteractive' />
-          <Script src="assets/js/main.js" strategy='beforeInteractive' />
+          <Script src="assets/js/jquery.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/jquery.poptrox.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/jquery.scrolly.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/jquery.scrollex.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/browser.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/breakpoints.min.js" strategy="beforeInteractive" />
+          <Script src="assets/js/util.js" strategy="beforeInteractive" />
+          <Script src="assets/js/main.js" strategy="beforeInteractive" />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
